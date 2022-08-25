524e65a38eff4280bf2bce45d66fd85c3e1833c8
Luciano Muniz <luciano.mo@gmail.com>
2022-08-25 19:46:30 -0300

    add grid layout component on landpage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles, useTheme } from '@material-ui/styles';
-import { Grid, Button, Typography } from '@material-ui/core';
+import { Grid, Button, Typography, useMediaQuery } from '@material-ui/core';
 
 import ButtonArrow from '../components/ui/ButtonArrow';
 import animationData from '../animations/landinganimation/data';
+import customSoftware from '../assets/Custom Software Icon.svg';
+import mobileAppsIcon from '../assets/mobileIcon.svg';
+import websiteIcon from '../assets/websiteIcon.svg';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
     maxWidth: '50em',
-    minWidth: '21em',
-    marginTop: '2em',
     marginLeft: '10%',
+    marginTop: '2em',
+    minWidth: '21em',
     [theme.breakpoints.down('sm')]: {
       maxWidth: '30em',
     },
   },
-
   buttonContainer: {
     marginTop: '1em',
   },
@@ -25,20 +27,36 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.common.orange,
     borderRadius: 50,
     height: 45,
-    width: 145,
     marginRight: 40,
+    width: 145,
     '&:hover': {
       backgroundColor: theme.palette.secondary.light,
     },
   },
+  heroTextContainer: {
+    marginLeft: '1em',
+    minWidth: '21.5em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 45,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
+  },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: 'none',
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
+    ...theme.typography.learnButton,
     fontSize: '0.9rem',
     height: 45,
     width: 145,
@@ -52,18 +70,25 @@ const useStyles = makeStyles((theme) => ({
       marginTop: '2em',
     },
   },
-  heroTextContainer: {
-    minWidth: '21.5em',
-    marginLeft: '1em',
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: 0,
+  servicesContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: '25px',
     },
+  },
+  specialText: {
+    color: theme.palette.common.orange,
+    fontFamily: 'Pacifico',
+  },
+  subtitle: {
+    marginBottom: '1em',
   },
 }));
 
 export default function LandingPage() {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const defaultOptions = {
     loop: true,
@@ -76,6 +101,7 @@ export default function LandingPage() {
 
   return (
     <Grid container direction='column' className={classes.mainContainer}>
+      {/* Hero Block */}
       <Grid item>
         <Grid container justify='flex-end' alignItems='center' direction='row'>
           <Grid sm item className={classes.heroTextContainer}>
@@ -107,6 +133,127 @@ export default function LandingPage() {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* Custom Software Block */}
+      <Grid item>
+        <Grid
+          container
+          direction='row'
+          justify={matchesSM ? 'center' : undefined}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>Custom Software Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Save Energy. Save Time. Save Money.
+            </Typography>
+            <Typography variant='subtitle1'>
+              Complete digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button variant='outlined' className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              src={customSoftware}
+              alt='custom software icon'
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* iOS/Android Block */}
+      <Grid item>
+        <Grid
+          container
+          direction='row'
+          justify={matchesSM ? 'center' : 'flex-end'}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>iOS/Android App Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Extend Functionality. Extend Access. Increase Engagement.
+            </Typography>
+            <Typography variant='subtitle1'>
+              Integrate your web experience or create a standalone app{' '}
+              {matchesSM ? null : <br />}with either mobile platform.
+            </Typography>
+            <Button variant='outlined' className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item style={{ marginRight: matchesSM ? 0 : '5em' }}>
+            <img
+              className={classes.icon}
+              src={mobileAppsIcon}
+              alt='mobile phone icon'
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* Website Block */}
+      <Grid item>
+        <Grid
+          container
+          direction='row'
+          justify={matchesSM ? 'center' : undefined}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>Website Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Reach More. Discover More. Sell More.
+            </Typography>
+            <Typography variant='subtitle1'>
+              Optimize for Search Engines, built for speed.
+            </Typography>
+            <Button variant='outlined' className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              src={websiteIcon}
+              alt='website icon'
+            />
           </Grid>
         </Grid>
       </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,39 +2,66 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const arcBlue = '#0B72B9';
 const arcOrange = '#FFBA60';
+const arcGrey = '#868686';
 
 export default createMuiTheme({
   palette: {
     common: {
-      blue: `${arcBlue}`,
-      orange: `${arcOrange}`,
+      blue: arcBlue,
+      orange: arcOrange,
     },
     primary: {
-      main: `${arcBlue}`,
+      main: arcBlue,
     },
     secondary: {
-      main: `${arcOrange}`,
+      main: arcOrange,
     },
   },
   typography: {
     tab: {
       fontFamily: 'Raleway',
-      textTransform: 'none',
-      fontWeight: 700,
       fontSize: '1rem',
+      fontWeight: 700,
+      textTransform: 'none',
     },
     estimate: {
+      color: 'white',
       fontFamily: 'Pacifico',
       fontSize: '1rem',
       textTransform: 'none',
-      color: 'white',
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      borderRadius: 50,
+      borderWidth: 2,
+      color: arcBlue,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+      textTransform: 'none',
     },
     h2: {
+      color: arcBlue,
       fontFamily: 'Raleway',
-      fontWeight: 700,
       fontSize: '2.5rem',
-      color: `${arcBlue}`,
+      fontWeight: 700,
       lineHeight: 1.5,
+      marginBottom: '1rem',
+    },
+    h3: {
+      color: arcBlue,
+      fontFamily: 'Pacifico',
+      fontSize: '2.5rem',
+    },
+    h4: {
+      color: arcBlue,
+      fontFamily: 'Raleway',
+      fontSize: '1.75rem',
+      fontWeight: 700,
+    },
+    subtitle1: {
+      color: arcGrey,
+      fontSize: '1.25rem',
+      fontWeight: 300,
     },
   },
 });
